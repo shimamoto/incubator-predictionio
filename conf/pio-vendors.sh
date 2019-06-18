3aff7a514c8373d66cdf8c718602c6fae3ceeb74
shimamoto <shimamoto@apache.org>
2019-06-18 10:46:07 +0900

    fixup

--- conf/pio-vendors.sh
+++ conf/pio-vendors.sh
@@ -36,7 +36,7 @@ if [ -z "$PIO_ELASTICSEARCH_VERSION" ]; then
 fi
 
 if [ -z "$PIO_HBASE_VERSION" ]; then
-    PIO_HBASE_VERSION="1.2.6"
+    PIO_HBASE_VERSION="1.4.10"
 fi
 
 export ES_IMAGE="docker.elastic.co/elasticsearch/elasticsearch"
